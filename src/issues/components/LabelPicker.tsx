82b7b148ce1cafc68f27307ea2f9a0f1a7d51db9
Gerardo Aguilar <gerardo.aguilar@unosquare.com>
2023-02-24 19:26:54 -0600

    Initial Commit

---
+++ src/issues/components/LabelPicker.tsx
@@ -0,0 +1,25 @@
+import { useQuery } from "@tanstack/react-query"
+
+export const LabelPicker = () => {
+  
+  const getLabels = async()=>{
+    const res = await fetch('https://api.github.com/repos/facebook/react/labels');
+    const data = await res.json();
+    console.log(data)
+    return data;
+  }
+
+  const labelsQuery=useQuery(['labels'], getLabels)
+
+  return (
+    <>
+        <span 
+            className="badge rounded-pill m-1 label-picker"
+            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+        >
+            Primary
+        </span>
+        
+    </>
+  )
+}
