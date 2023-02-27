7e6d19fc4c06fa8e57a7a33c2a4c16520cd03721
Gerardo Aguilar <gerardo.aguilar01@outlook.com>
2023-02-26 23:00:07 -0600

    Added bearer token

--- src/api/GitHubApi.ts
+++ src/api/GitHubApi.ts
@@ -2,5 +2,5 @@ import axios from "axios";
 
 export const GitHubApi = axios.create({
    baseURL: 'https://api.github.com/repos/facebook/react',
-   headers: {}
+   headers: { Authorization: 'Bearer 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' }
 })
